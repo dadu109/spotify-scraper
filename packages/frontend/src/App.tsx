88737bd5923c29ae216de80dde7aa7633ee9effb
d.miczek <d.miczek@ideo.pl>
2020-12-26 15:06:24 +0100

    Squashed commit of the following:
    commit cb88e04074473db461a063256283337c5e4e2688
    Author: d.miczek <d.miczek@ideo.pl>
    Date:   Sat Dec 26 14:58:45 2020 +0100
    
        feat: setup eslint frontend
    
    commit bc3fa2f4a9b17bb214994f436184e4e5509291d6
    Author: d.miczek <d.miczek@ideo.pl>
    Date:   Sat Dec 26 02:10:49 2020 +0100
    
        feat: fenerate frontend with by CRA with typescript

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      hello world
+    </header>
+  </div>
+);
+
+export default App;
