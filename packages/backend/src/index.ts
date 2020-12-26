0f254cb22ebc6fa068f13ac72080af78baf0e6eb
d.miczek <d.miczek@ideo.pl>
2020-12-26 15:40:37 +0100

    feat: expres setup with ts and eslint

---
+++ packages/backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", ( req: any, res: any ) => {
+    res.send( "Hello dsa!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
